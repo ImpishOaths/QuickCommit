d3e615fa295b3f72e51747489048e6a2046995be
ImpishOaths <Sophia@impishoaths.dev>
2024-12-29 16:47:16 -0800

    2024-12-29

--- QuickCommit.py
+++ QuickCommit.py
@@ -1,7 +1,12 @@
 import subprocess
+from datetime import datetime
 from easygui import *
+date = datetime.today().strftime('%Y-%m-%d')
 msg = enterbox("Commit Message")
+if(msg != ""):
+    msg += "\n"
+msg += date
 subprocess.call(["git","add","."])
 subprocess.call(["git","commit","-m",msg])
 subprocess.call(["git","push"])
-msgbox("done!")
+msgbox("Done!")
