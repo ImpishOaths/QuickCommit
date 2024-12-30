092cbc46f31df414f1faebab9366036a5dcdaa63
ImpishOaths <Sophia@impishoaths.dev>
2024-12-29 16:36:39 -0800

    test commit

---
+++ QuickCommit.py
@@ -0,0 +1,6 @@
+import subprocess
+from easygui import *
+msg = enterbox("Commit Message")
+subprocess.call(["git","add","."])
+subprocess.call(["git","commit","-m",msg])
+subprocess.call(["git","push"])
