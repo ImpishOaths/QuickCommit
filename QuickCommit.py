67391a49995567640d3a7be67974d2b4bb25f213
ImpishOaths <Sophia@impishoaths.dev>
2025-01-01 17:21:16 -0800

    Added noCommit Logic

--- QuickCommit.py
+++ QuickCommit.py
@@ -1,11 +1,13 @@
 import subprocess
 from datetime import datetime
 from easygui import *
-date = datetime.today().strftime('%Y-%m-%d')
-msg = enterbox("Commit Message")
-if(msg != ""):
-    msg += "\n"
-msg += date
+msg = enterbox("Commit message:")
+if(msg == ""):
+    date = datetime.today().strftime('%Y-%m-%d')
+    msg = enterbox("Explain no commit:")
+    with open("noCommit.log", "a") as noCommit:
+        noCommit.write(msg)
+        noCommit.write("- " + date + "\n")
 subprocess.call(["git","add","."])
 subprocess.call(["git","commit","-m",msg])
 subprocess.call(["git","push"])
