2da828ef5247dece5fbca89ebcd978854e455e7c
ImpishOaths <Sophia@impishoaths.dev>
2025-01-01 17:21:46 -0800

    Testing Again

--- QuickCommit.py
+++ QuickCommit.py
@@ -7,7 +7,7 @@ if(msg == ""):
     msg = enterbox("Explain no commit:")
     with open("noCommit.log", "a") as noCommit:
         noCommit.write(msg)
-        noCommit.write("- " + date + "\n")
+        noCommit.write(" - " + date + "\n")
 subprocess.call(["git","add","."])
 subprocess.call(["git","commit","-m",msg])
 subprocess.call(["git","push"])
